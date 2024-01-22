a93af3b7b1d5dfb873f8aa4247ac0aae36bc8329
SubhamChowdhuryRiju <subhamchowdhury1025@gmail.com>
2024-01-22 21:05:01 +0530

    Update V2.1

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['SECRET_KEY'] = 'MARGDARSHAN'
 db_name = 'database.db'
 db_path = os.path.join(os.getcwd(), db_name)
 
+
 def create_table():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -24,43 +25,51 @@ def create_table():
     conn.commit()
     conn.close()
 
+
 create_table()
+
 
 # Function to generate a salt (a random value to make each password hash unique)
 def generate_salt():
     return os.urandom(16)  # 16 bytes (128 bits) of random data
 
+
 # Function to hash a password with a salt
 def hash_password(password, salt):
     # Combine the salt and the password
     salted_password = salt + password.encode()
-    
+
     # Hash the salted password using SHA-256 (you can use other hash functions like SHA-512)
     hashed_password = hashlib.sha256(salted_password).hexdigest()
-    
+
     return hashed_password
+
 
 # Function to verify a password
 def verify_password(stored_password, input_password, salt):
     hashed_input_password = hash_password(input_password, salt)
     return stored_password == hashed_input_password
 
+
 salt = generate_salt()
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', 
-            roadmap_data = data.roadmap_data, 
-            blog_data = data.blog_data,
-            predictor_data = data.predictor_data,
-            study_material_data = data.study_material_data,
-            test_series_data = data.test_series_data)
+    return render_template('index.html',
+                           roadmap_data=data.roadmap_data,
+                           blog_data=data.blog_data,
+                           predictor_data=data.predictor_data,
+                           study_material_data=data.study_material_data,
+                           test_series_data=data.test_series_data)
+
 
 @app.route('/blog')
 def blog():
-    return render_template('blog2.html', blog_data = data.blog_data)
+    return render_template('blog2.html', blog_data=data.blog_data)
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login_page', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['email']
@@ -93,13 +102,15 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/logout')
+
+@app.route('/logout_page')
 def logout():
     session.pop('username', None)
     # flash('Logged out successfully', 'success')
     return redirect(url_for('login'))
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route('/register_page', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['email']
@@ -109,20 +120,20 @@ def register():
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
         existing_user = cursor.fetchone()
+        conn.close()
 
         if existing_user:
             # flash('Username already exists', 'danger')
             return '<h1>User Already Exists</h1>'
         else:
-            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, 
-                            hash_password(password, salt)))
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username,
+                                                                                    hash_password(password, salt)))
             conn.commit()
             # flash('Registration successful', 'success')
             return redirect(url_for('login'))
 
-        conn.close()
-
     return render_template('sign_up.html')
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/js/chatbot.js
@@ -0,0 +1,79 @@
+const chatbotConatainer = document.querySelector(".chatbot-container");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input #send-btn");
+const maximiseBtn = document.querySelector(".maximise-btn");
+const minimiseBtn = document.querySelector(".minimise-btn");
+
+let userMessage = null;
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+
+  if (className === "outgoing") {
+    const chatP = document.createElement("p");
+    chatP.textContent = message;
+    chatLi.appendChild(chatP);
+  } else {
+    const imgTag = `<img src="../static/images/botimg.svg" class="chat-robot"/>`;
+    const chatP = document.createElement("p");
+    chatP.textContent = message;
+    chatLi.innerHTML = imgTag;
+    chatLi.appendChild(chatP);
+  }
+
+  return chatLi;
+};
+
+const handleChat = () => {
+  userMessage = chatInput.value; // Get user entered message and remove extra whitespace
+  console.log(userMessage);
+  if (!userMessage) return;
+
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
+    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    // generateResponse(incomingChatLi);
+  }, 600);
+  chatInput.value = "";
+};
+
+sendChatBtn.addEventListener("click", handleChat);
+
+closeBtn.addEventListener("click", () => {
+  chatbotConatainer.classList.remove("show-chatbot");
+  chatbotToggler.style.opacity = "1";
+});
+
+chatbotToggler.addEventListener("click", () => {
+  chatbotConatainer.classList.add("show-chatbot");
+  chatbotToggler.style.opacity = "0";
+});
+
+maximiseBtn.addEventListener("click", () => {
+  chatbotConatainer.classList.add("fullscreen");
+  maximiseBtn.style.opacity = "0";
+  minimiseBtn.style.opacity = "1";
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+});
+
+minimiseBtn.addEventListener("click", () => {
+  chatbotConatainer.classList.remove("fullscreen");
+  minimiseBtn.style.opacity = "0";
+  maximiseBtn.style.opacity = "1";
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+});
